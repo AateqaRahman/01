5695e076f774112d8acd5e460d71eb501dc0e5a8
AateqaRahman <rxnz2576@leeds.ac.uk>
2025-02-16 23:02:22 +0000

    work already

--- task2/vpet.py
+++ task2/vpet.py
@@ -18,7 +18,7 @@ class VirtualPet:
         self.energy_levels += 10
 
     def __str__(self):
-        return f"{self.name_of_pet} with {self.energy_levels} energy level and {self.hunger_levels} hunger level"
+        return f"{self.name_of_pet} with {self.energy_levels} energy points and {self.hunger_levels} hunger level"
         
         
 pass
