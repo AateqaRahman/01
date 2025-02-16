6fb000ff23759d7f3a01ca3b31a27c7da1d5ce03
AateqaRahman <rxnz2576@leeds.ac.uk>
2025-02-16 22:47:54 +0000

    pleaseeeeee

---
+++ task2/vpet.py
@@ -0,0 +1,24 @@
+class VirtualPet:
+    def __init__(self,name,energy=10,hunger=0):
+        self.name_of_pet = name 
+        self.energ_levels = energy
+        self.hunger_levels = hunger
+    
+    def play(self):
+        if self.energy_levels < 2:
+            print ("TOO tired to play")
+        else:
+            self.energy_level -= 2
+            self.hunger_levels += 2
+
+    def feed(self):
+        self.hunger_levels = max (0, self.hunger_levels - 3)
+        
+    def sleep(self):
+        self.energy_levels += 10
+
+    def __str__(self):
+        return f"{self.name_of_pet} with {self.energ_levels} energy level and {self.hunger_levels} hunger level"
+        
+        
+pass
